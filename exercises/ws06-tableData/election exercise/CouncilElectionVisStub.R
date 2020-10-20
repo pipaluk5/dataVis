8bc3bdddbd8b356e4d319dac7160d0ac5a3297bd
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-10-20 11:02:58 +0200

    election council vis stub

---
+++ exercises/ws06-tableData/election exercise/CouncilElectionVisStub.R
@@ -0,0 +1,8 @@
+# london council election exercise script stub ---------------
+
+
+library(tidyverse)
+library(here)
+
+df<-read.csv(here("exercises","ws06-tableData","election exercise","london_council_election_2014_ward.csv"))
+
