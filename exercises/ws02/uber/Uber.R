008675fa312e21834468811308854f93a86ef8fd
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-10-20 10:41:51 +0200

    changes to dirs

--- exercises/ws02/uber/Uber.R
+++
@@ -1,9 +0,0 @@
-library(here)
-library(tidyverse)
-tu<- read_table2("exercises/ws02/uber/nyuber.dat.csv")
-
-ggplot(tu, aes(x = uber, y = taxi)) +
-  geom_point(aes(size = rides)) +
-  geom_abline(slope = -1, linetype = 2) +
-  geom_smooth(method = "lm", se = FALSE, color = "red") +
-  scale_size_area()
--- exercises/ws03/uber/Uber.R
+++
@@ -1,9 +0,0 @@
-if (! file.exists("nyuber.dat"))
-  download.file("http://www.stat.uiowa.edu/~luke/data/nyuber.dat",
-                "nyuber.dat")
-tu <- read.table("nyuber.dat", head = TRUE)
-ggplot(tu, aes(x = uber, y = taxi)) +
-  geom_point(aes(size = rides)) +
-  geom_abline(slope = -1, linetype = 2) +
-  geom_smooth(method = "lm", se = FALSE, color = "red") +
-  scale_size_area()
