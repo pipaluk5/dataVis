818d0d0c3829d09d11dc3d7ef80f772ec1e0e0eb
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-10-20 11:49:03 +0200

    create the within and overall cols

--- exercises/ws06-tableData/election exercise/CouncilElectionVisStub.R
+++ exercises/ws06-tableData/election exercise/CouncilElectionVisStub.R
@@ -6,3 +6,5 @@ library(here)
 
 df<-read.csv(here("exercises","ws06-tableData","election exercise","london_council_election_2014_ward.csv"))
 
+df <- df %>% group_by(Ward_code,party) %>% arrange(candidate) %>% mutate(position_within=1:n()) %>% ungroup()
+df <- df %>% group_by(Ward_code) %>% arrange(candidate) %>% mutate(position_overall=1:n()) %>% ungroup()
